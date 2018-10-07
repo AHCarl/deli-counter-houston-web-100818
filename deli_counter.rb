df04f88d0ab83a2ff2641671cb4a96922f9bcb68
Alexander Carlin <ahcarlin@gmail.com>
2018-10-07 02:04:03 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -18,11 +18,10 @@ def take_a_number(katz_deli, name)
 end
 
 def now_serving(katz_deli)
-  if katz_deli.size == 0 
+  if katz_deli.empty? 
     puts "There is nobody waiting to be served!"
+  else
+    name = katz_deli.first 
+    puts "Now serving #{name}."
   end
-  
-  name = katz_deli.first 
-  puts "Now serving #{name}."
-end
 end
