f0b65d7fe643dee3c685aaec85250f261a63327e
Alexander Carlin <ahcarlin@gmail.com>
2018-10-07 01:38:32 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,8 +4,9 @@ def line(katz_deli)
   if katz_deli.length == 0 
     puts "The line is currently empty."
   end
-  puts "The line is currently: " + katz_deli.each_with_index do |name, index| + "#{index+1}. #{name}"
-end
+  else 
+    puts "The line is currently: " + katz_deli.each_with_index do |name, index| + "#{index+1}. #{name}"
+  end
 end
 
 def take_a_number(katz_deli, name)
