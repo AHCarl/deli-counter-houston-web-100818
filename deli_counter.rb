c6d1016055fdc3bca3ecaca0af92d1407697f067
Alexander Carlin <ahcarlin@gmail.com>
2018-10-07 01:37:16 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,13 +4,20 @@ def line(katz_deli)
   if katz_deli.size == 0 
     puts "The line is currently empty."
   end
-  katz_deli.each_with_index do |name, index|
-    puts "The line is currently: #{index+1}. #{name}"
-  end
-    
+  puts "The line is currently: " + katz_deli.each_with_index do |name, index| + "#{index+1}. #{name}"
+end
 end
 
 def take_a_number(katz_deli, name)
   katz_deli << name
   puts "Welcome, #{name}. You are number #{katz_deli.index(name+1)} in line."
+end
+
+def now_serving(katz_deli)
+  if katz_deli.size == 0 
+    puts "There is nobody waiting to be served!"
+  end
+  
+  name = katz_deli.first 
+  puts "Now serving #{name}."
 end
