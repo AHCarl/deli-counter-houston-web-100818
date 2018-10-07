7cce72437c49bba099644f0fb569fd41b7a748bd
Alexander Carlin <ahcarlin@gmail.com>
2018-10-07 02:01:18 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,10 +4,10 @@ def line(katz_deli)
   if katz_deli.empty?
     puts "The line is currently empty."
   else
-  a = "The line is currently: "
+  a = "The line is currently:"
   b = ""
   katz_deli.each_with_index do |name, index| 
-    b += "#{index+1}. #{name} "
+    b += " #{index+1}. #{name}"
   end
   puts a + b
 end
